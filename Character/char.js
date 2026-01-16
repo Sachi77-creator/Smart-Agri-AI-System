21fde9bcec3caeb417e65ca3048872b656c47397
Sachi77-creator <sachin07senthilkumar@gmail.com>
2026-01-16 13:14:03 +0530

    char

---
+++ Character/char.js
@@ -0,0 +1,15 @@
+const textarea = document.getElementById("text");
+const totalChars = document.getElementById("para");
+const remainingChars = document.getElementById("count");
+
+const maxLength = 200; 
+textarea.addEventListener("input", () => {
+    const currentLength = textarea.value.length; 
+    totalChars.textContent = `Total Characters: ${currentLength}`;
+    remainingChars.textContent = `Remaining: ${maxLength - currentLength}`;
+
+    if (currentLength > maxLength) {
+        alert(`You cannot type more than ${maxLength} characters!`);
+        textarea.value = textarea.value.substring(0, maxLength);
+    }
+});
